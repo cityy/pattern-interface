a33c4a18fb1009c3a251eafb8e2ee90fe0929f20
cityy <ferdinand.list@googlemail.com>
2017-05-09 18:35:27 +0200

    Nodes and Edges initialize properly from the database. Connect Nodes uses saves edges to the database. ToDo: addNode() saves edges to the database, function to delete edges, function to edit nodes

--- app.js
+++ app.js
@@ -12,9 +12,6 @@ var app = express();
 
 var oDB = require('./sv_orientdb.js');
 
-//console.log(oDB.db);
-//var vis_ = require('./src/js/vis_network.js');
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,29 +21,114 @@ app.set('view engine', 'pug');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-// OrientDB Routes
 
+
+
+// =========================
+// OrientDB Routes
+// =========================
+
+// HANDLE REQUEST FOR ALL NODES
 app.get('/nodes', function(req, res){ // request for all the nodes
 	oDB.db.class.get('pattern').then( function(pattern){
 		pattern.list().then( function(records){ // promises records of class pattern, records is an associative array
-			//console.log ( records );
 			res.send( records );
 		});
 	}); // db.class.get('pattern')
 });
 
-app.get('/nodes/:clusterId/:recordId', function(req, res){
+// HANDLE REQUEST FOR ALL EDGES
+app.get('/edges', function(req, res){
+	oDB.db.class.get('patternconnections').then(function(patternconnections){
+		patternconnections.list().then(function(records){
+			/*for(var i in records){
+				console.log('Edge #' + i + ' with id: ' + records[i]['@rid']);
+				console.log('From: #'+ records[i].out.cluster + ':' + records[i].out.position);
+				console.log('To: #'+ records[i].in.cluster + ':' + records[i].in.position);
+				console.log('================================');
+			}*/
+			res.send( records );
+		});
+	});
+});
+
+
+// HANDLE A SINGLE RECORD REQUEST
+app.get('/nodes/:clusterId/:recordId', function(req, res){ 
 	oDB.db.class.get('pattern').then( function(pattern){
 		pattern.list().then( function(records){ // records is an associative array
+			res.send( records );
 		});
 	}); // db.class.get('pattern')	
 }); // app.get(/nodes/:clusterId/:recordId)
+
+
+// HANDLE ADDNODE REQUEST
+app.post('/nodes', function(req, res){
+	oDB.db.class.get('pattern').then(function(pattern){
+		pattern.create({
+			id: req.body.id,
+			name: req.body.name,
+			label: req.body.label
+		}).then( function(newPattern) {
+			res.send('Pattern ' + newPattern.name + ' with the id ' + newPattern.id + ' added to the database.');
+		});
+	});
+});
+
+
+// HANDLE DELETENODES REQUEST
+app.delete('/nodes', function(req, res){
+	var ids = req.body.ids; // array of ids
+	var promises = []; // for collection of all promises
+
+	function deleteNode(id){ 
+    // promise that deletes a vertex from orientDB
+		return new Promise(function(resolve, reject){
+			oDB.db.delete('VERTEX', 'pattern').where('id=' + id).one();
+			resolve(id); // promise return value
+		}); 
+	}
+  	// for earch id
+	for ( var i in ids ) {
+    // push the deleteNode promise to the promises array
+		promises.push( deleteNode(ids[i]));
+	}
+  	//Promise.all() resolves when all promises in the array resolved
+	Promise.all(promises).then(function(dataArr){ 
+		res.send('Deleted the following Patterns: ' + dataArr);
+	}).catch(function(err){ console.log(err) });
+}); // app.delete(...)
+
+
+// HANDLE CONNECTNODES REQUESTS
+app.post('/edges', function(req, res){
+	//console.log( req.body );
+
+	let fromId = req.body.from;
+	let toId = req.body.to;
+
+	let fromNode = oDB.db.select().from('pattern').where({id: fromId}).all().then(function(selectFrom){
+		let toNode = oDB.db.select().from('pattern').where({id: toId}).all().then(function(selectTo){
+			let fromRid = '#' + selectFrom[0]['@rid'].cluster + ':' + selectFrom[0]['@rid'].position;
+			let toRid = '#' + selectTo[0]['@rid'].cluster + ':' + selectTo[0]['@rid'].position;
+
+			oDB.db.create('EDGE', 'patternconnections').from(fromRid).to(toRid).set({
+				fromId: fromId,
+				toId: toId,
+			}).one().then( function(newEdge){
+				res.send('Created Edge:' + newEdge);
+			});
+		});
+	});
+});
+
 
 
 // catch 404 and forward to error handler
--- src/js/vis_network.js
+++ src/js/vis_network.js
@@ -1,86 +1,101 @@
 var vis = require('vis');
-var nodesObj, edgesObj, currentNodeCount, network;
+var currentNodeCount, network, currentIds;
 network = {};
+currentIds = [];
+var nodesObj = {};
+var edgesObj = {};
 
 /* ==================================================
 /* CREATE THE NODES & EDGES ARRAYS USING DataSet()
 /* ==================================================
 */
 
-function getNodes(url){
-    return new Promise( function (resolve, reject){
-        let visDbReq = new XMLHttpRequest(); // http request for getting nodes from the database
-        visDbReq.open('GET', url, true); // initialize the request
-        
-        visDbReq.onload = function(){ // check the status
-            if ( visDbReq.status == 200 ){ resolve(visDbReq.response); }
-            else{ reject(Error(visDbReq.statusText)); }
-        };
-        visDbReq.onerror = function(){ // handle network errors
-            reject(Error('Network Error'));
-        };
-        visDbReq.send(null); // send the request, takes Parameters for POST requests                
-    });
-} // getNodes(url);
+function getAll(url){ // url is an array
+    return new Promise( function (resolve, reject) {
+        var f = {
+            send: function(i){
+                var promise = new Promise(function(resolve, reject){
+                    var visDbReq = new XMLHttpRequest();
+                    visDbReq.open('GET', url[i]);
+                    visDbReq.onload = function(){
+                        if (visDbReq.status === 200) { resolve(visDbReq.responseText); }
+                        else { console.log('Error' + visDbReq.statusText); }
+                    }
+                    visDbReq.send(null);
+                }); // var promise
+                return promise;
+            } // function(i)
+        } // obj f
 
-getNodes('/nodes').then(function(response){
-    let allNodes = JSON.parse( response );
-    let nodesArray, edgesArray;
-    nodesArray = []; // array of Node Objects
-    edgesArray = []; // array of Edge Objects
-    currentNodeCount = allNodes.length;
-    //console.log( currentNodeCount );
+        // array that holds all the request promises
+        var promises = []; 
+        // for each requested url, push one response
+        for (let i = 0; i < url.length; i++){
+            promises.push( f.send(i) );
+        }
+        // if all sub promises are done, resolve the getAll promise
+        Promise.all(promises).then(function(dataArr){
+            resolve(dataArr); 
+        });
+    }); // return
+} // getAll(url);
 
+
+
+getAll( ["/nodes", "/edges"] ).then(function(responseArr){
+    let allNodes = JSON.parse( responseArr[0] );
+    let allEdges = JSON.parse ( responseArr[1] );
+    var nodesArray = []; // array of Node Objects
+    var edgesArray = []; // array of Edge Objects
+    // if there is nodes, set current node counter - else set current node counter to 0
+    if(allNodes.length){ currentNodeCount = allNodes.length; }
+    else{ currentNodeCount = 0; }
+    // loop through the nodes responded by the server
     for ( let i = 0; i < currentNodeCount; i++ ){
-        // objects to push to the nodes/edges arrays
-        let node = {}; 
-        let edge = {};
-
         // create initial Nodes
+        let node = {};
         node.id = allNodes[i].id; // first nodeId = 0, second nodeId = 1, ...
         node.label = allNodes[i].label + ' id #' + allNodes[i].id;
         //console.log(node);
         nodesArray.push( node ); // append each i to the nodes Array
-
-        // create initial edges
-        if (i > 0) {
-            edge.from = i-1; // set previous node as edge start
-            edge.to = i;  // set this node as edge end
-            edgesArray.push( edge ); // push edge to edges Array, produces a linear connection of initial nodes
-        } //  if i > 0
     } // for i < currentNodeCount
+    // create initial edges
+    if (allEdges) {
+        for(let i = 0; i < allEdges.length; i++){
+            /*console.log('EDGE:' + allEdges[i]['@rid']);
+            console.log('From:' + allEdges[i].out);
+            console.log('To:' + allEdges[i].in);
+            console.log(JSON.stringify(allEdges[i]));*/
+            let edge = {};
+            edge.from = allEdges[i].fromId;
+            edge.to = allEdges[i].toId;
+            edgesArray.push( edge );
+        } // for
+    } //  if i > 0
+    console.log( edgesArray );
     // convert Edges and Nodes Arrays to Objects
-    nodesObj = new vis.DataSet(nodesArray);
+    nodesObj = new vis.DataSet( nodesArray );
     edgesObj = new vis.DataSet(edgesArray);
 
-    /* ==================================================
-    /* GET THE NODE NETWORK CONTAINER DIV
-    /* ==================================================
-    */
-
+    // ==================================================
+    // GET THE NODE NETWORK CONTAINER DIV
+    // ==================================================
+    
     var container = document.getElementById('patternLanguage');
 
-    /* ==================================================
-    /* 05 GATHER THE NODES/EDGES DATA AND SET OPTIONS
-    /* ==================================================
-    */
+    // ==================================================
+    // 05 GATHER THE NODES/EDGES DATA AND SET OPTIONS
+    // ==================================================
 
     var data = {
         nodes: nodesObj,
         edges: edgesObj
     };
 
-
     var options = {
         interaction:{
             multiselect: true,
         },
-        layout: {
-            /*hierarchical: {
-                direction: 'UD'
-            }*/
-        },
-        // canvas option
         configure: {                 // http://visjs.org/docs/network/configure.html
             enabled: false,         // toggle the config interface
             filter: true,
@@ -113,7 +128,7 @@ getNodes('/nodes').then(function(response){
             selectionWidth: 0,
             color: {
                 color: '#010101',
-                highlight: '#010101'
+                highlight: '#107896'
             },
             scaling:{
             }
@@ -123,39 +138,32 @@ getNodes('/nodes').then(function(response){
             avoidOverlap: 0.5,      // control the node overlap 0...1 
           }  
         },
-        /* Other Modules:
-        /* edges: {}                // http://visjs.org/docs/network/edges.html
-        /* groups: {}               // http://visjs.org/docs/network/groups.html
-        /* interaction: {}          // http://visjs.org/docs/network/interaction.html
-        /* layout: {}               // http://visjs.org/docs/network/layout.html
-        /* manipulation: {}         // http://visjs.org/docs/network/manipulation.html
-        /*
-        /* Other Otions:
-        /* autoResize: true         // detect when the container is resized
-        /* height:
-        /* width:
-        /* locale: en               // language
-        /* locales:                 // custom translations
-        /* clickToUse:              // network only handles events when :active
-        */ 
-
+        // Other Modules:
+        // edges: {}                // http://visjs.org/docs/network/edges.html
+        // groups: {}               // http://visjs.org/docs/network/groups.html
+        // interaction: {}          // http://visjs.org/docs/network/interaction.html
+        // layout: {}               // http://visjs.org/docs/network/layout.html
+        // manipulation: {}         // http://visjs.org/docs/network/manipulation.html
+        //
+        // Other Otions:
+        // autoResize: true         // detect when the container is resized
+        // height:
+        // width:
+        // locale: en               // language
+        // locales:                 // custom translations
+        // clickToUse:              // network only handles events when :active
     };
 
-    /* ==================================================
-    /* 06 INITIALIZE THE GRAPH USING vis.Network()
-    /* ==================================================
-    */
+    // ==================================================
+    // 06 INITIALIZE THE GRAPH USING vis.Network()
+    // ==================================================
 
     network = new vis.Network(container, data, options);
-    //console.log(network);
-
-    console.log(network);
 
     // NETWORK SELECT EVENTS
     network.on('selectNode', function(){ checkHudDisable(); });
     network.on('deselectNode', function(){ checkHudDisable(); });
-}); //getNodes()
-
+}); //getAll()
 
 function checkHudDisable() { // check if HUD buttons need to be disabled, depending on what is selected
     let selectedNodes = network.getSelectedNodes();
@@ -185,42 +193,113 @@ function checkHudDisable() { // check if HUD buttons need to be disabled, depend
             $('#deleteNodes').removeClass('disabled'); // enable delete
             document.getElementById("connectNodes").className = " disabled"; // disable connect
     } //switch
-} // checkHudDisable();
+} // checkHudDisable();     
 
-//MODULE EXPORTS
+
 var exports = module.exports = {};
 exports.network = network;
 exports.addNode = function addNode() {
-                    let newId = currentNodeCount++;
-                    currentNodeCount++;
-                    let selectedNodes = network.getSelectedNodes(); // array
-                    let newSelection = [newId];
+                getAll(['/nodes']).then(function(response){
+                    let allNodes = JSON.parse( response[0] );
+                    let newId;
+                    // if there are nodes
+                    if(currentNodeCount > 0){
+                        // get the currently used ids
+                        for (let i=0; i < currentNodeCount; i++ ) {
+                            currentIds[i] = allNodes[i].id;
+                        }
+                        // sort currently used ids in descending order
+                        currentIds.sort(function(a,b){return b-a});
+                        newId = currentIds[0] + 1;
+                    }
+                    // if there are no nodes start with id=0
+                    else{ newId = 0; }
+                    // new node's data object
+                    let data = { 
+                        id: newId,
+                        name: 'newpattern' + currentNodeCount,
+                        label: 'I was added to the database dynamically.'
+                    };
 
-                    nodesObj.add({ id:newId, label: "New Pattern id:" + newId });
-                    if( selectedNodes ){
+                    // create the http post request
+                    let postNodeReq = new XMLHttpRequest();
+                    postNodeReq.open('POST', '/nodes', true);
+                    postNodeReq.setRequestHeader("Content-type", "application/json; charset=utf-8");
+
+                    postNodeReq.addEventListener('load', function(event) {
+                          if (postNodeReq.status >= 200 && postNodeReq.status < 300) {
+                             console.log(postNodeReq.responseText);
+                          } else {
+                             console.warn(postNodeReq.statusText, postNodeReq.responseText);
+                          }
+                    });
+                    // send the request, parameters have to be in json format
+                    postNodeReq.send( JSON.stringify(data) ); 
+
+                    // create the new node in frontend
+                    nodesObj.add({ id: data.id, label: data.label + " id #" + data.id }); 
+                    currentNodeCount++;
+
+                    let selectedNodes = network.getSelectedNodes(); // array
+                    let newSelection = [data.id]; // add new node to selection
+
+                    // if a node is selected, automatically connect the new node
+                    if( selectedNodes ){ 
                         for (let i = 0; i < selectedNodes.length; i++){
                             newSelection.push(selectedNodes[i]);
-                            edgesObj.add({from: selectedNodes[i], to: newId});
+                            edgesObj.add({from: selectedNodes[i], to: data.id});
                         }
                     }
 
                     network.setSelection({nodes: newSelection,});
                     checkHudDisable();
-                };
+                });
+}
 
 exports.deleteSelectedNode = function deleteSelectedNode(){
-                    let selectedNodes = network.getSelectedNodes();
+                    let selectedNodes = network.getSelectedNodes(); // array containing ids
+                    let deleteNodesReq = new XMLHttpRequest();
+                    let data = { ids: selectedNodes }; 
+
+                    deleteNodesReq.open('DELETE', '/nodes', true);
+                    deleteNodesReq.setRequestHeader("Content-type", "application/json; charset=utf-8");
+                    deleteNodesReq.addEventListener('load', function(event) {
+                          if (deleteNodesReq.status >= 200 && deleteNodesReq.status < 300) {
+                             console.log(deleteNodesReq.responseText);
+                          } else {
+                             console.warn(deleteNodesReq.statusText, deleteNodesReq.responseText);
+                          }
+                    });
+                    // send the request, parameters have to be in json format
+                    deleteNodesReq.send( JSON.stringify(data) ); 
+
                     for (let i=0; i < selectedNodes.length; i++){
-                        nodesObj.remove({id: selectedNodes[i]});    
+                        nodesObj.remove({id: selectedNodes[i]}); // remove each selected node in frontend
+                        currentNodeCount--; // decrease nodecount for each deleted node
                     }
                     checkHudDisable();
                     
-                };
+                }
 
 exports.connectSelectedNodes = function connectSelectedNodes(){
-                    if( network.getSelectedNodes() ){
-                        let selectedNodes = network.getSelectedNodes();
-                        let i = 0;
-                        edgesObj.add({from: selectedNodes[i], to: selectedNodes[i+1]});
+                    let connectNodesReq = new XMLHttpRequest();
+                    let selectedNodes = network.getSelectedNodes();
+                    let data = { 
+                        from: selectedNodes[0],
+                        to: selectedNodes[1]
                     }
-                };
+                    connectNodesReq.open('POST', '/edges', true);
+                    connectNodesReq.setRequestHeader("Content-type", "application/json; charset=utf-8");
+
+                    connectNodesReq.addEventListener('load', function(event) {
+                          if (connectNodesReq.status >= 200 && connectNodesReq.status < 300) {
+                             console.log(connectNodesReq.responseText);
+                          } else {
+                             console.warn(connectNodesReq.statusText, connectNodesReq.responseText);
+                          }
+                    });
+                    // send the request, parameters have to be in json format
+                    connectNodesReq.send( JSON.stringify(data) ); 
+                    // connect nodes in frontend
+                    edgesObj.add({from: data.from, to: data.to});
+                }
